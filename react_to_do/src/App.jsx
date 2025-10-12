950cbed1558cc34462898a553b2051ca3a856aa4
= <=>
2025-10-12 02:02:03 -0400

    s1

---
+++ react_to_do/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Task List</h1>
+      <div>
+        <p>Learn React</p>
+        <p>Build a project</p>
+        <p>Deploy to production</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
